a74ce413e40cc82f3227280ce2db250fa99e4cdf
Nam.Nguyen12 (Nate.Nguyen)/Sotatek Frontend Developer <153987205+NamNguyenVan1601@users.noreply.github.com>
2025-05-19 17:34:08 +0700

    Fix auto-reload issue in micro-frontend environment

--- next.config.js
+++ next.config.js
@@ -6,13 +6,24 @@ const nextConfig = {
   // the canvas package for webpack
   // https://github.com/mozilla/pdf.js/issues/16214
   output: 'standalone',
-  webpack: (config) => {
+  assetPrefix: "/resume-builder-static",
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/resume-builder-static/_next/:path+',
+          destination: '/_next/:path+',
+        },
+      ],
+    }
+  },
+  webpack: (config, { dev, isServer }) => {
     // Setting resolve.alias to false tells webpack to ignore a module
     // https://webpack.js.org/configuration/resolve/#resolvealias
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
     return config;
-  },
+  }
 };
 
 module.exports = nextConfig;
